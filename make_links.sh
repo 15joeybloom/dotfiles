9e98a26883cc01c4b8a4d19e86258b8a67d14660
Joey Bloom <15joeybloom@gmail.com>
2019-11-30 13:27:24 -0600

    Cool bash prompt stuff!

--- make_links.sh
+++ make_links.sh
@@ -4,4 +4,5 @@ ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/.gitignore_global ~/.gitignore_global
 ln -sf ~/dotfiles/.spacemacs ~/.spacemacs
 ln -sf ~/dotfiles/.bash_aliases ~/.bash_aliases
+ln -sf ~/dotfiles/.bash_prompt ~/.bash_prompt
 #ln -sf ~/dotfiles/ ~/
