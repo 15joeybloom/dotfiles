07dbdc159df25e0a195afaa384cfad1115340bd6
15joeybloom <jbloom@opploans.com>
2019-12-26 11:14:10 -0600

    make .bash_prompt pure

--- make_links.sh
+++ make_links.sh
@@ -4,5 +4,6 @@ ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/.gitignore_global ~/.gitignore_global
 ln -sf ~/dotfiles/.spacemacs ~/.spacemacs
 ln -sf ~/dotfiles/.bash_aliases ~/.bash_aliases
+ln -sf ~/dotfiles/.opploans_aliases ~/.opploans_aliases
 ln -sf ~/dotfiles/.bash_prompt ~/.bash_prompt
 #ln -sf ~/dotfiles/ ~/
