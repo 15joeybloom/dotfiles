f0b66707a4d3494fdbb4734a46370dfb281c6f94
Joey Bloom <joey@joey-mbair.local>
2022-04-03 21:57:55 -0700

    rm opploans

--- bash_profile_snippet.sh
+++ bash_profile_snippet.sh
@@ -1,9 +1,6 @@
 if [ -f ~/.bash_aliases ]; then
     . ~/.bash_aliases
 fi
-if [ -f ~/.opploans_aliases ]; then
-    . ~/.opploans_aliases
-fi
 if [ -f ~/.bash_prompt ]; then
     . ~/.bash_prompt
     export PS1=`get_iterm2_prompt`
